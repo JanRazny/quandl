4ee870052f678323b81e878304afef417104319d
Jan Razny <netorg@gmx.de>
2022-06-17 08:46:17 +0200

    test1

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const API_KEY = 'Yow5_-xD1PUpJYrDYjVu';
 
+
 async function loadCourse() {
-    let url = 'https://data.nasdaq.com/api/v3/datasets/BITFINEX/LUNAF0USTF0?start_date=2022-05-22&end_date=2022-05-22&api_key=Yow5_-xD1PUpJYrDYjVu';
+    let today = new Date();
+    //today.setDate(new Date().getDate() - 1);
+    //let startDate = today.toISOString().split('T')[0];
+    //let endDate = today.toISOString().split('T')[0];
+    let url = `https://data.nasdaq.com/api/v3/datasets/BCHAIN/MKPRU?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`;
     let response = await fetch(url);
     let responseAsJson = await response.json();
-    console.log('API answer: ', responseAsJson['dataset']['data'][0][4]);
+
+    console.log(responseAsJson);
+
+    showBlockChainToDay(responseAsJson);
+}
+
+
+function showBlockChainToDay(responseAsJson) {
+    let bitcoinToDay =  responseAsJson['dataset']['data'][0][1];
+    document.getElementById('blockchainInUSDToday').innerHTML = bitcoinToDay;
+}
+
+
+function datePicker() {
+    let startDate = document.getElementById('startDate').value;
+    startDate.setDate(new Date().getDate() - 1);
 }
